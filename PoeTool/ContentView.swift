8502708d98e066917b11d94e23a74b8520fd7c85
KaFn <niuwencong1@gmail.com>
2019-10-13 21:27:24 +0800

    Initial Commit

---
+++ PoeTool/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PoeTool
+//
+//  Created by 高梵 on 2019/10/13.
+//  Copyright © 2019 KaFn. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
