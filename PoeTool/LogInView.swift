56c9705eae2b0aa61f17acfb66500b181f0731fe
KaFn <niuwencong1@gmail.com>
2019-10-14 08:45:52 +0800

    added codable struct

--- PoeTool/LogInView.swift
+++ PoeTool/LogInView.swift
@@ -7,19 +7,40 @@
 //
 
 import SwiftUI
+struct AccountInfo : Codable
+{
 
+}
+struct characterInfo : Codable
+{
+    var name : String
+    var league : String
+    var className : String
+    var level : Int
+    private enum CodingKeys : String, CodingKey {
+        case name, league, className = "class", level
+    }
+}
 struct LogInView: View {
-    @State private var accName = ""
+    @State private var accName = "niuwencong1"
+    @State private var POESSID = "f2b5f9a200793c5b0f33ad660f8b31a8"
     var body: some View {
         VStack
         {
             TextField("Account Name",text: $accName)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding()
+            TextField("POESSID",text: $POESSID)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+//            Button(action: <#T##@escaping () -> Void##@escaping () -> Swift.Void#>, label: <#T##() -> Label##() -> Label#>)
         }
     }
 }
+func LogIn(accName:String, POESSID:String, completion: @escaping (NSArray)-> Void)
+{
 
+}
 struct LogInView_Previews: PreviewProvider {
     static var previews: some View {
         LogInView()
