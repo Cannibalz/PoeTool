698c38f1cd5bca0f6e2b7e2971bf8e65dfe7c807
KaFn <niuwencong1@gmail.com>
2019-10-14 10:11:05 +0800

    use closure to get and parse json

--- PoeTool/LogInView.swift
+++ PoeTool/LogInView.swift
@@ -7,10 +7,6 @@
 //
 
 import SwiftUI
-struct AccountInfo : Codable
-{
-
-}
 struct characterInfo : Codable
 {
     var name : String
@@ -33,13 +29,42 @@ struct LogInView: View {
             TextField("POESSID",text: $POESSID)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .padding()
-//            Button(action: <#T##@escaping () -> Void##@escaping () -> Swift.Void#>, label: <#T##() -> Label##() -> Label#>)
+            Button(action: {
+                LogIn(accName: self.accName, POESSID: self.POESSID)
+                { charactersInfo in
+                    print(charactersInfo.count)
+                    print(charactersInfo)
+                
+                }
+            })
+            {
+                Text("Log In")
+            }
+
         }
     }
 }
-func LogIn(accName:String, POESSID:String, completion: @escaping (NSArray)-> Void)
+func LogIn(accName:String, POESSID:String, completion:@escaping ([characterInfo])->())
 {
-
+    let session = URLSession(configuration: .default)
+    let urlStirng = URL(string: "https://www.pathofexile.com/character-window/get-characters?accountName=\(accName)")
+    let urlReq = URLRequest(url: urlStirng!)
+    let decoder = JSONDecoder()
+    let task = session.dataTask(with: urlReq)
+    { (data, responds, error) in
+        if let data = data
+        {
+            do
+            {
+                let charactersInfo = try decoder.decode([characterInfo].self, from: data)
+                return completion(charactersInfo)
+            }
+            catch
+            {
+                print(error)
+            }
+        }
+    }.resume()
 }
 struct LogInView_Previews: PreviewProvider {
     static var previews: some View {
