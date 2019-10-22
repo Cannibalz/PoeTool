776e7a5cef5022558071f16c459799cbab214454
KF <niuwencong1@gmail.com>
2019-10-22 12:30:46 +0800

    moving state to vm

--- PoeTool/LogInView.swift
+++ PoeTool/LogInView.swift
@@ -5,14 +5,11 @@
 //  Created by 高梵 on 2019/10/13.
 //  Copyright © 2019 KaFn. All rights reserved.
 //
-
 import SwiftUI
 
 
 
 struct LogInView: View {
-    @State private var accName = "niuwencong1"
-    @State private var POESSID = "f2b5f9a200793c5b0f33ad660f8b31a8"
     @State private var segue : Int? = 0
     @State var accountInfo : AccountInfo = AccountInfo()
     @State var remeberPass = true
@@ -23,27 +20,32 @@ struct LogInView: View {
         {
             VStack
             {
-                NavigationLink(destination: CharacterSelectView(account:self.$accountInfo), tag: 1, selection: $segue) {
+                NavigationLink(destination: CharacterSelectView(account:self.$accountInfo), tag: 200, selection: $viewModel.statusCode) {
                     EmptyView()
                 }
-                TextField("Account Name",text: $accName)
+                TextField("Account Name",text: $viewModel.accName)
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                         .padding()
-                TextField("POESSID",text: $POESSID)
+                TextField("POESSID",text: $viewModel.POESSID)
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                         .padding()
-                Button(action: {
-                    self.viewModel.accountAuth(accName: self.accName, POESESSID: self.POESSID)
-                })
-                {
-                    Text("\(viewModel.statusCode)")
-                }
                 
-                Toggle(isOn: $remeberPass) {
+                
+                Toggle(isOn: $viewModel.isToggle) {
                     Text("Remeber me")
                 }
                 .frame(width: 200.0)
-                
+                NavigationLink(destination: CharacterSelectView(account:self.$accountInfo), isActive: $viewModel.authed, label: {
+                    Button(action: {
+                        self.viewModel.accountAuth(accName: self.viewModel.accName, POESESSID: self.viewModel.POESSID)
+                        {statusCode in
+                            print(statusCode)
+                        }
+                    })
+                    {
+                        Text("Authenticate")
+                    }
+                })
             }
         }
 
--- PoeTool/LoginViewModel.swift
+++ PoeTool/LoginViewModel.swift
@@ -3,15 +3,20 @@ import Combine
 
 class LoginViewModel: ObservableObject
 {
-    @Published var statusCode : Int = 0
-    
-    func accountAuth(accName:String, POESESSID:String)
+    var accName = "niuwencong1"
+    var POESSID = "f2b5f9a200793c5b0f33ad660f8b31a8"
+    var statusCode : Int? = 0
+    var isToggle : Bool = false
+    var authed : Bool = false
+    func accountAuth(accName:String, POESESSID:String,Completion:@escaping(Int)->())
     {
         PoEAPI().isValid(accName: accName, POESESSID: POESESSID, Completion:
         {statusCode in
             self.statusCode = statusCode
+            Completion(statusCode)
         })
     }
+    
 }
 
 
