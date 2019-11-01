3f65878df7db278f590c8361b728befa36854725
KF <niuwencong1@gmail.com>
2019-11-01 09:43:54 +0800

    save before reformatting

--- PoeTool/CharacterDetailView.swift
+++ PoeTool/CharacterDetailView.swift
@@ -10,70 +10,69 @@ import Foundation
 import SwiftUI
 import Combine
 
+let cellSize = CGFloat(30)
 
-struct CharacterDetailView:View
-{
-    @ObservedObject var viewModel : CharacterDetailViewModel
-    init()
-    {
+struct CharacterDetailView: View {
+    @ObservedObject var viewModel: CharacterDetailViewModel
+
+    init() {
         self.viewModel = CharacterDetailViewModel()
         //self.viewModel = CharacterDetailViewModel(char: charInfo)
     }
+
     @State var menuOpen = false
-    var body: some View
-    {
-        ZStack
-        {
-            List{
-                    
-                
-                VStack
-                {
+    var body: some View {
+        ZStack {
+            VStack {
+                VStack {
                     EquipmentlView()
-                    ZStack
-                    {
-                        VStack(spacing:34)
-                        {
-                            ForEach(0..<3){_ in Divider()}
+                    ZStack {
+                        VStack(spacing: 30) {
+                            ForEach(0..<3) { _ in
+                                Divider()
+                            }
                         }
-                        HStack(spacing:34)
-                        {
-                            ForEach(0..<6){_ in Divider()}
+                        HStack(spacing: 30) {
+                            ForEach(0..<6) { _ in
+                                Divider()
+                            }
                         }
-                    }.frame(width: 170, height: 68)
-                    ZStack
-                    {
-                        VStack(spacing:20)
-                        {
-                            ForEach(0..<6){_ in Divider()}
+                    }.frame(width: 30 * 5, height: 30 * 2)
+                    ZStack {
+                        VStack(spacing: 30) {
+                            ForEach(0..<6) { _ in
+                                Divider()
+                            }
                         }
-                        HStack(spacing:20)
-                        {
-                            ForEach(0..<13){_ in Divider()}
+                        HStack(spacing: 30) {
+                            ForEach(0..<13) { _ in
+                                Divider()
+                            }
                         }
-                        ForEach(self.viewModel.mainInventory)
-                        {item in
-                            URLImage(URL(string: item.icon)!,processors: [Resize(size: CGSize(width: item.w*20, height: item.h*20), scale: UIScreen.main.scale)],content:{$0.image.resizable().aspectRatio(contentMode: .fill).clipped()}).frame(width:CGFloat(item.w*20),height: CGFloat(item.h*20))
-                                .position(x: CGFloat(item.x*20), y: CGFloat(item.y*20))
+                        ForEach(self.viewModel.mainInventory) {
+                            item in
+                            URLImage(URL(string: item.icon)!, processors: [Resize(size: CGSize(width: item.w * 30, height: item.h * 30), scale: UIScreen.main.scale)], content: { $0.image.resizable().aspectRatio(contentMode: .fill).clipped() }).frame(width: CGFloat(item.w * 20), height: CGFloat(item.h * 30))
+                                    .position(x: CGFloat(item.x * 30), y: CGFloat(item.y * 30))
                         }
-                        
-                    }.frame(width:20*12,height: 20*5)
-            }
-            .navigationBarItems(trailing: Button(action:
-            {
-                self.openMenu()
-                }, label: {Image(systemName: "info.circle")}))
-            .navigationBarTitle(Text(viewModel.selectCharacter!.name).font(.system(size: 10)), displayMode: .inline)
+
+                    }.frame(width: 20 * 12, height: 20 * 5)
                 }
+                        .navigationBarItems(trailing: Button(action: {
+                            self.openMenu()
+                        }, label: {
+                            Image(systemName: "info.circle")
+                        }))
+                        .navigationBarTitle(Text(viewModel.selectCharacter!.name).font(.system(size: 10)), displayMode: .inline)
+            }
             SideMenu(width: 200, isOpen: self.menuOpen, menuClose: self.openMenu)
-        }.onAppear{
-            self.viewModel.getItems()
-        }
-        .onDisappear
-        {
-            
-        }
+        }.onAppear {
+                    self.viewModel.getItems()
+                }
+                .onDisappear {
+
+                }
     }
+
     func openMenu() {
         self.menuOpen.toggle()
     }
@@ -81,8 +80,9 @@ struct CharacterDetailView:View
 
 #if DEBUG
 var char = CharacterInfo()
+
 struct CharacterDetailView_Previews: PreviewProvider {
-    
+
     static var previews: some View {
         CharacterDetailView()
     }
--- PoeTool/EquipmentView.swift
+++ PoeTool/EquipmentView.swift
@@ -14,17 +14,17 @@ struct EquipmentlView:View
     {
         ZStack
         {
-            VStack(spacing:34)
+            VStack(spacing:20)
             {
                 ForEach(0..<7){_ in Divider()}
             }
-            HStack(spacing:34)
+            HStack(spacing:20)
             {
                 ForEach(0..<9){_ in Divider()}
             }
-            URLImage(URL(string: "https://web.poecdn.com/image/Art/2DItems/Armours/Boots/Alberonswarpath.png?scale=1&w=2&h=2&v=354e2b293d5c3878e906730b17d2cb6d")!,processors: [Resize(size: CGSize(width: 68, height: 68), scale: UIScreen.main.scale)],content:{$0.image.resizable().aspectRatio(contentMode: .fill).clipped()}).frame(width:68,height: 68)
-                .position(x: 34, y: 34)
-        }.frame(width: 272, height: 204)
+            URLImage(URL(string: "https://web.poecdn.com/image/Art/2DItems/Armours/Boots/Alberonswarpath.png?scale=1&w=2&h=2&v=354e2b293d5c3878e906730b17d2cb6d")!,processors: [Resize(size: CGSize(width: 40, height: 40), scale: UIScreen.main.scale)],content:{$0.image.resizable().aspectRatio(contentMode: .fill).clipped()}).frame(width:40,height: 40)
+                .position(x: 20, y: 20)
+        }.frame(width: 20*8, height: 20*6)
     }
 }
 class EquipmentViewModel : ObservableObject
